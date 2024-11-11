87bb76a17ae8e6ae08d55d65f42d733769d44a05
Lucas Marcal <lucasmmarcal@gmail.com>
2024-11-10 21:19:23 -0300

    Implementado metodo delete

--- src/main/java/com/estudos/curso/resources/UserResource.java
+++ src/main/java/com/estudos/curso/resources/UserResource.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.estudos.curso.services.UserService;
@@ -38,5 +41,23 @@ public class UserResource {
 		User getUserById = service.FindById(id);
 		return ResponseEntity.ok().body(getUserById);
 	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		
+		obj = service.insert(obj);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		
+		service.delete(id);
+		// noContent() retorna resposta vazia
+		return ResponseEntity.noContent().build();
+		
+	}
+	
+	
 
 }
--- src/main/java/com/estudos/curso/services/UserService.java
+++ src/main/java/com/estudos/curso/services/UserService.java
@@ -31,4 +31,14 @@ public class UserService {
 		
 	}
 	
+	public User insert(User obj) {
+		
+		return userRepository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		
+		userRepository.deleteById(id);
+		
+	}
 }
